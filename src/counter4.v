552abe49477af75f3ed977c0c1d827562deb2fa5
Alexi del Rosario <92180363+alexijustine@users.noreply.github.com>
2026-03-09 15:17:35 -0700

    Update counter4.v
    DFF -> Always block

--- src/counter4.v
+++ src/counter4.v
@@ -15,13 +15,15 @@ module counter4(
     wire [3:0] mux_out_w;       // mux output
     wire [3:0] add_sub_out_w;   // addSub8 output
     wire [3:0] q_w;
+	reg [3:0] q_r; // register for FF
     
     // clock enable FF if one is active
     wire ce_w = (up_i & ~dw_i) | (dw_i & ~up_i) | ld_i;
     
     wire sub = ~up_i & dw_i;  // 1 if decrement/DW
     
-    assign q_w = q_o;
+    assign q_w = q_r;
+	assign q_o = q_r;
     
     addSub8 inc_dec (
         .A(q_w),          // current counter val
@@ -34,38 +36,13 @@ module counter4(
     assign mux_out_w[1] = (ld_i & din_i[1]) | (~ld_i & add_sub_out_w[1]);
     assign mux_out_w[2] = (ld_i & din_i[2]) | (~ld_i & add_sub_out_w[2]);
     assign mux_out_w[3] = (ld_i & din_i[3]) | (~ld_i & add_sub_out_w[3]);
-    
-    FDRE #(.INIT(1'b0) ) a_ff_0 (
-        .C(clk_i),          // Clock
-        .R(reset_i),           // reset == 0
-        .CE(ce_w),          // Clock Enable?
-        .D(mux_out_w[0]),        
-        .Q(q_o[0])
-   );
-   
-   FDRE #(.INIT(1'b0) ) a_ff_1 (
-        .C(clk_i),          // Clock
-        .R(reset_i),           // reset == 0
-        .CE(ce_w),          // Clock Enable?
-        .D(mux_out_w[1]),        
-        .Q(q_o[1])
-   );
-   
-   FDRE #(.INIT(1'b0) ) a_ff_2 (
-        .C(clk_i),          // Clock
-        .R(reset_i),           // reset == 0
-        .CE(ce_w),          // Clock Enable?
-        .D(mux_out_w[2]),        
-        .Q(q_o[2])
-   );
-   
-   FDRE #(.INIT(1'b0) ) a_ff_3 (
-        .C(clk_i),          // Clock
-        .R(reset_i),           // reset == 0
-        .CE(ce_w),          // Clock Enable?
-        .D(mux_out_w[3]),        
-        .Q(q_o[3])
-   );
+
+    always @(posedge clk_i) begin
+        if (reset_i)
+            q_r <= 4'b0000;
+        else if (ce_w)
+            q_r <= mux_out_w;
+    end
    
    assign utc_o = q_o[0] & q_o[1] & q_o[2] & q_o[3];
    assign dtc_o = ~q_o[0] & ~q_o[1] & ~q_o[2] & ~q_o[3];
